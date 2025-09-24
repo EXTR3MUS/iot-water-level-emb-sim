1532371b9085a17eb6c5b9ac7f066de460b0e156
EXTR3MUS <diego7042@gmail.com>
2025-09-24 20:49:05 -0300

    feat: implement sensor reading with pthreads

--- src/main.c
+++ src/main.c
@@ -1,4 +1,27 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void* read_sensor(void *arg);
 
 int main(){
+    pthread_t thread_1, thread_2;
+
+    pthread_create(&thread_1, NULL, &read_sensor, NULL);
+    pthread_join(thread_1, NULL);
+
     return 0;
+}
+
+void* read_sensor(void *arg){
+    srand(time(NULL)); // Seed the random number generator
+
+    while(1){
+         int random_number = (rand() % 100) + 1; // 1 to 100
+
+        sleep(5);
+        printf("Current level: %d \n", random_number);
+    }
 }
