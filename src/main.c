8f41ba222e3a3d37e6580314adbe6bad8b4c8a60
EXTR3MUS <diego7042@gmail.com>
2025-09-26 13:21:30 -0300

    feat: add save_data thread to handle data saving functionality

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 void* read_sensor(void *arg);
 void* send_data(void *arg);
+void* save_data(void *arg);
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_t thread1, thread2;
@@ -13,13 +14,15 @@ pthread_t thread1, thread2;
 int random_number = 0;
 
 int main(){
-    pthread_t thread_1, thread_2;
+    pthread_t thread_1, thread_2, thread_3;
 
     pthread_create(&thread_1, NULL, &read_sensor, NULL);
     pthread_create(&thread_2, NULL, &send_data, NULL);
+    pthread_create(&thread_3, NULL, &save_data, NULL);
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
+    pthread_join(thread_3, NULL);
 
     return 0;
 }
@@ -31,7 +34,17 @@ void* read_sensor(void *arg){
     while(1){
         pthread_mutex_lock(&mutex1);
         random_number  = (rand() % 100) + 1; // 1 to 100
+        printf("Reading data: Current level: %d \n", random_number);
         pthread_mutex_unlock(&mutex1);  
+        sleep(5);
+    }
+}
+
+void* save_data(void *arg){
+    while(1){
+        pthread_mutex_lock(&mutex1);
+        printf("Saving data: Current level: %d \n", random_number);
+        pthread_mutex_unlock(&mutex1);
         sleep(5);
     }
 }
@@ -40,7 +53,7 @@ void* read_sensor(void *arg){
 void* send_data(void *arg){
     while(1){
         pthread_mutex_lock(&mutex1);
-        printf("Current level: %d \n", random_number);
+        printf("Sending data: Current level: %d \n", random_number);
         pthread_mutex_unlock(&mutex1);
         sleep(5);
     }
